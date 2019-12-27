068dbf21f6ad45ec37b358175bf978a52792a6c6
Bertrik Sikken <bertrik@sikken.nl>
2019-12-28 00:24:29 +0100

    Protect framebuffer.h against multiple inclusion.

--- framebuffer.h
+++ framebuffer.h
@@ -1,3 +1,6 @@
+#ifndef _FRAMEBUFFER_H_
+#define _FRAMEBUFFER_H_
+
 #include <stdint.h>
 
 #define LED_NUM_ROWS    7
@@ -8,4 +11,6 @@ typedef struct {
     uint8_t g;
     uint8_t b;
 } pixel_t;
+
+#endif /* _FRAMEBUFFER_H_ */
 
