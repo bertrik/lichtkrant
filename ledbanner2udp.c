e63f781cae252f1ca8d00e042e127e6f66d14981
Bertrik Sikken <bertrik@sikken.nl>
2025-11-02 22:42:34 +0100

    Update ledbanner2udp to use multicast.

--- ledbanner2udp.c
+++ ledbanner2udp.c
@@ -4,7 +4,8 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <strings.h>
+#include <string.h>
+#include <stdbool.h>
 #include <arpa/inet.h>
 
 static uint16_t rgb888_to_rgb565(const char *rgb)
@@ -16,11 +17,10 @@ static uint16_t rgb888_to_rgb565(const char *rgb)
     return rgb565;
 }
 
-#define NUM_PIXELS (80*8)
+#define NUM_PIXELS (80 * 8)
 #define PORT 1565
-#define IP_ADDR "10.42.43.53"
+#define DEFAULT_IP "239.0.0.1"
 
-// overwrite input buffer
 static void convert_frame(char *buffer)
 {
     for (int n = 0; n < NUM_PIXELS; ++n) {
@@ -31,30 +31,62 @@ static void convert_frame(char *buffer)
     }
 }
 
+// Helper: detect if IPv4 address is multicast (224.0.0.0–239.255.255.255)
+static bool is_multicast_address(uint32_t addr)
+{
+    uint8_t first_octet = (addr >> 24) & 0xFF;
+    return (first_octet >= 224 && first_octet <= 239);
+}
+
 int main(int argc, char *argv[])
 {
     int sockfd;
-    struct sockaddr_in servaddr;
+    struct sockaddr_in destaddr;
+    const char *ip_addr = (argc > 1) ? argv[1] : DEFAULT_IP;
 
-    const char *ip_addr = IP_ADDR;
-    if (argc > 1) {
-        ip_addr = argv[1];
+    memset(&destaddr, 0, sizeof(destaddr));
+    destaddr.sin_family = AF_INET;
+    destaddr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, ip_addr, &destaddr.sin_addr) <= 0) {
+        perror("Invalid IP address");
+        return 1;
     }
 
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_addr.s_addr = inet_addr(ip_addr);
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_family = AF_INET;
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    uint32_t ip_be = ntohl(destaddr.sin_addr.s_addr);
+    bool is_multicast = is_multicast_address(ip_be);
 
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    if (is_multicast) {
+        // Configure multicast TTL = 1 (stay in local network)
+        unsigned char ttl = 1;
+        if (setsockopt(sockfd, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl)) < 0) {
+            perror("setsockopt(IP_MULTICAST_TTL)");
+        }
+        printf("→ Sending MULTICAST to %s:%d\n", ip_addr, PORT);
+    } else {
+        printf("→ Sending UNICAST to %s:%d\n", ip_addr, PORT);
+    }
+
+    // Main loop
     while (1) {
         char buffer[NUM_PIXELS * 3];
-        read(STDIN_FILENO, buffer, sizeof(buffer));
-        write(STDOUT_FILENO, buffer, sizeof(buffer));
+        ssize_t r = read(STDIN_FILENO, buffer, sizeof(buffer));
+        if (r <= 0)
+            break;
+
+        write(STDOUT_FILENO, buffer, r);
 
         convert_frame(buffer);
-        send(sockfd, buffer, NUM_PIXELS * 2, 0);
+        sendto(sockfd, buffer, NUM_PIXELS * 2, 0, (struct sockaddr *)&destaddr, sizeof(destaddr));
     }
+
+    close(sockfd);
     return 0;
 }
